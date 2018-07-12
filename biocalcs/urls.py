c2dc3d6c1679848b18dcacedfea1ced1eddcc00f
Matthew <matthew@mttmartin.com>
2018-07-11 20:38:44 -0700

    Automatically use seq_calcs for now

--- biocalcs/urls.py
+++ biocalcs/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('seq_calcs.urls')),
     path('seq_calcs/', include('seq_calcs.urls')),
     path('admin/', admin.site.urls),
 ]
